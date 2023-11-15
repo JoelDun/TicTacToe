e091c3ab0c8f68d450b612512b944ecb3d4b1058
Joel <Joel.dun@pm.me>
2023-11-15 16:05:57 -0500

    Create initial files

---
+++ script.js
@@ -0,0 +1,50 @@
+// Store the gameboard as an array inside of an object Gameboard
+function Gameboard() {
+  const rows = 3;
+  const columns = 3;
+  const board = [];
+
+  // Create a 2d array that will represent the state of the game board
+  // For this 2d array, row 0 will represent the top row and
+  // column 0 will represent the left-most column.
+  // This nested-loop technique is a simple and common way to create a 2d array.
+  for (let i = 0; i < rows; i++) {
+    board[i] = [];
+    for (let j = 0; j < columns; j++) {
+      board[i].push(Cell());
+    }
+  }
+
+// Create players inside of objects
+
+
+
+// Create an object to help control the flow of the game
+function GameController(
+  playerOneName = "Player One",
+  playerTwoName = "Player Two"
+) {
+  const board = Gameboard()
+  
+  const players = [
+    {
+      name: playerOneName,
+      token: 1
+    },
+    {
+      name: playerTwoName,
+      token: 2
+    }
+  ];
+
+  let activePlayer = players[0];
+
+  const switchPlayerTurn = () => {
+    activePlayer = activePlayer === players[0] ? players[1] : players[0];
+  };
+  const getActivePlayer = () => activePlayer;
+
+  const printNewRound = () => {
+    board.printBoard();
+    console.log(`${getActivePlayer().name}'s turn.`);
+  };
