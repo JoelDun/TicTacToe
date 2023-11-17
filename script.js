95f7edc5947c61733941a962f245f1a8d34d38f9
Joel <Joel.dun@pm.me>
2023-11-17 10:25:01 -0500

    Start fresh from the ground up

--- script.js
+++ script.js
@@ -1,75 +1,16 @@
-// Store the gameboard as an array inside of an object Gameboard
 function Gameboard() {
-  const rows = 3;
-  const columns = 3;
-  const board = [];
-
-  // Create a 2d array that will represent the state of the game board
-  // For this 2d array, row 0 will represent the top row and
-  // column 0 will represent the left-most column.
-  // This nested-loop technique is a simple and common way to create a 2d array.
-  for (let i = 0; i < rows; i++) {
-    board[i] = [];
-    for (let j = 0; j < columns; j++) {
-      board[i].push(Cell());
-    }
-  }
-
-
-// Create an object to help control the flow of the game
-function GameController(
-  playerOneName = "Player One",
-  playerTwoName = "Player Two"
-) {
-  const board = Gameboard()
-  
-  const players = [
-    {
-      name: playerOneName,
-      token: 1
-    },
-    {
-      name: playerTwoName,
-      token: 2
-    }
+  const board = [
+    [null, null, null],
+    [null, null, null],
+    [null, null, null],
   ];
 
-  let activePlayer = players[0];
-
-  const switchPlayerTurn = () => {
-    activePlayer = activePlayer === players[0] ? players[1] : players[0];
-  };
-  const getActivePlayer = () => activePlayer;
-
-  const printNewRound = () => {
-    board.printBoard();
-    console.log(`${getActivePlayer().name}'s turn.`);
-  }
-
-  const playRound = (column) => {
-    // Drop a token for the current player
-    console.log(
-      `Dropping ${getActivePlayer().name}'s token into column ${column}...`
-    );
-    board.dropToken(column, getActivePlayer().token);
-
-    /*  This is where we would check for a winner and handle that logic,
-        such as a win message. */
-
-    // Switch player turn
-    switchPlayerTurn();
-    printNewRound();
-  };
-
-  // Initial play game message
-  printNewRound();
-
-  // For the console version, we will only use playRound, but we will need
-  // getActivePlayer for the UI version, so I'm revealing it now
   return {
-    playRound,
-    getActivePlayer
+    getBoard: () => board,
+    // Add more methods as needed
   };
 }
 
-const game = GameController();
+// Test the Gameboard
+const gameboard = Gameboard();
+console.log(gameboard.getBoard()); // Should log the initial empty game board
