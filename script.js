9f7b84b55d40a7c6df70e9a4e27615292ea3d560
Joel <Joel.dun@pm.me>
2023-12-16 14:59:41 -0500

    Fix restart button

--- script.js
+++ script.js
@@ -25,7 +25,6 @@ function GameController(player1, player2) {
     playTurn: (row, col) => {
       if (board.getBoard()[row][col] === null) {
         board.getBoard()[row][col] = activePlayer.marker;
-        // Switch player for the next turn
         activePlayer = activePlayer === player1 ? player2 : player1;
       } else {
         console.log("Invalid move. Cell already occupied.");
@@ -38,10 +37,11 @@ function GameController(player1, player2) {
 
 const player1 = Player("Player 1", "X");
 const player2 = Player("Player 2", "O");
-const gameController = GameController(player1, player2);
+let gameController = GameController(player1, player2);
 
 function createGameBoard() {
   const gameBoard = document.getElementById('game-board');
+  gameBoard.innerHTML = '';
 
   for (let row = 0; row < 3; row++) {
     for (let col = 0; col < 3; col++) {
@@ -60,134 +60,65 @@ function createCell(row, col) {
   return cell;
 }
 
-
 function handleCellClick(event) {
   const clickedCell = event.target;
+  const row = parseInt(clickedCell.dataset.row);
+  const col = parseInt(clickedCell.dataset.col);
 
   if (!clickedCell.textContent) {
+    gameController.playTurn(row, col);
     clickedCell.textContent = gameController.getActivePlayer().marker;
 
-    // Check for a winner after placing the current player's marker
-    if (checkForWinner()) {
+    if (checkForWinner(gameController.getActivePlayer().marker)) {
       console.log(`${gameController.getActivePlayer().name} wins!`);
     } else if (checkForTie()) {
       console.log("It's a tie!");
-    } else {
-      gameController.playTurn(parseInt(clickedCell.dataset.row), parseInt(clickedCell.dataset.col));
-
-      // Switch to the next player
-      const nextPlayer = gameController.getActivePlayer();
-
-      // Check for a winner after switching to the next player
-      if (checkForWinner()) {
-        console.log(`${nextPlayer.name} wins!`);
-      } else if (checkForTie()) {
-        console.log("It's a tie!");
-      }
     }
   } else {
     console.log("Invalid move. Cell already occupied.");
   }
 }
 
-
-
 function checkForWinner(marker) {
   const board = gameController.getBoard();
+  return checkRowsForWinner(board, marker) || 
+         checkColumnsForWinner(board, marker) || 
+         checkDiagonalsForWinner(board, marker);
+}
 
-  // Check rows
-  for (let i = 0; i < board.length; i++) {
-    if (checkRow(i, marker)) {
+function checkRowsForWinner(board, marker) {
+  return board.some(row => row.every(cell => cell === marker));
+}
+
+function checkColumnsForWinner(board, marker) {
+  for (let col = 0; col < board.length; col++) {
+    if (board.every(row => row[col] === marker)) {
       return true;
     }
   }
-
-  // Check columns
-  for (let i = 0; i < board.length; i++) {
-    if (checkColumn(i, marker)) {
-      return true;
-    }
-  }
-
-  // Check diagonals
-  return checkDiagonal(marker);
+  return false;
 }
 
-
-function checkRow(row, marker) {
-  const board = gameController.getBoard();
-  const firstCell = board[row][0];
-  if (firstCell === null) {
-    return false;
-  }
-  return board[row].every(cell => cell === marker);
+function checkDiagonalsForWinner(board, marker) {
+  const mainDiagonal = board.every((row, idx) => row[idx] === marker);
+  const antiDiagonal = board.every((row, idx) => row[board.length - 1 - idx] === marker);
+  return mainDiagonal || antiDiagonal;
 }
-
-function checkColumn(col, marker) {
-  const board = gameController.getBoard();
-  const firstCell = board[0][col];
-  if (firstCell === null) {
-    return false;
-  }
-  for (let i = 1; i < board.length; i++) {
-    if (board[i][col] !== marker) {
-      return false;
-    }
-  }
-  return true;
-}
-
-function checkDiagonal(marker) {
-  const board = gameController.getBoard();
-  const mainDiagonal = [board[0][0], board[1][1], board[2][2]];
-  if (mainDiagonal.every(cell => cell === marker)) {
-    return true;
-  }
-
-  const antiDiagonal = [board[0][2], board[1][1], board[2][0]];
-  return antiDiagonal.every(cell => cell === marker);
-}
-
 
 function checkForTie() {
   const board = gameController.getBoard();
   return board.every(row => row.every(cell => cell !== null));
 }
 
-document.addEventListener('DOMContentLoaded', function () {
-  createGameBoard();
-
- // Get the restart button element
- const restartButton = document.getElementById('restart-button');
- // Add a click event listener to the restart button
- restartButton.addEventListener('click', function () {
-  // Call a function to restart the game
-  restartGame();
-});
-});
-
-
 function restartGame() {
-  // Clear the game board by removing text content from each cell
-  const cells = document.querySelectorAll('.cell');
-  cells.forEach(cell => {
-    cell.textContent = '';
-  });
-
-  // Reset the internal game state (clear the board)
-  const board = gameController.getBoard();
-  for (let i = 0; i < board.length; i++) {
-    for (let j = 0; j < board[i].length; j++) {
-      board[i][j] = null;
-    }
-  }
-
-  // Switch back to the initial active player
-  const player1 = Player("Player 1", "X");
-  const player2 = Player("Player 2", "O");
-  gameController.getActivePlayer() === player1 ? (activePlayer = player1) : (activePlayer = player2);
-
-  // Optional: Display a message indicating the game has been restarted
+  createGameBoard();
+  gameController = GameController(player1, player2);
   console.log("Game restarted!");
 }
 
+document.addEventListener('DOMContentLoaded', function () {
+  createGameBoard();
+
+  const restartButton = document.getElementById('restart-button');
+  restartButton.addEventListener('click', restartGame);
+});
